3da220f7fbbb2f5f3b7e2d7a75f129c26ac722e4
BialyMT92 <mt.bialek92@gmail.com>
2025-11-28 12:48:35 +0100

    bbb

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "ci_cd_bucket" {
-  bucket = "bialy-my-terraform-ci-cd-bucket-example" 
+  bucket = "bialy-terraform-ci-cd-bucket-example" 
   tags = {
     Name        = "bialy_CiCdBucket"
     ManagedBy   = "bialy_GitHubActions"
