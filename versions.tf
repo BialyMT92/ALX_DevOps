28e6826a4dbf9409055dc4f0ababf8a79f4d657a
BialyMT92 <mt.bialek92@gmail.com>
2025-11-28 12:41:01 +0100

    tfstate to aws

--- versions.tf
+++ versions.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket = "bialy-my-terraform-ci-cd-bucket-example"
+    key    = "./terraform.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
