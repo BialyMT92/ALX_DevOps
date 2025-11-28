33c186c57841a423640bead569e6096817eb247c
BialyMT92 <mt.bialek92@gmail.com>
2025-11-28 14:28:37 +0100

    jenkins

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "ci_cd_bucket" {
-  bucket = "bialy-terraform-ci-cd-bucket-example" 
+  bucket = "jenkins-bialy-terraform-ci-cd-bucket-example" 
   tags = {
     Name        = "bialy_CiCdBucket"
     ManagedBy   = "bialy_GitHubActions"
